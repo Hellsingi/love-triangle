4f52afa796c24b253094f5d9685461d93000ab78
Hellsingi <f.c.realmadrid@mail.ru>
2018-09-22 19:01:31 +0300

    Solve lobe triangle

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
+
 /**
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
- */
-module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+*/
+
+module.exports=function getLoveTrianglesCount(preferences=[])
+{
+var counter=0,firstIndex,secondIndex,thirdIndex,firstValue,secondValue,thirdValue;
+for (var i = 0; i < preferences.length; i++)
+{
+    firstValue=preferences[i];
+    firstIndex=i+1;
+    secondIndex=firstValue-1;
+    secondValue=preferences[secondIndex];
+    thirdIndex=secondValue-1;
+    thirdValue=preferences[thirdIndex];
+    if ( thirdValue==firstIndex)   
+    {
+        counter++;
+    }
+      
+}
+return Math.trunc(counter/3) ;
 };
